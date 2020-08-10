ec997b4f0d369680725419e6a02acf0033a48646
Zan Zibert <zan_zibert@hotmail.com>
2020-08-10 15:09:01 +0200

    creating the goodbye handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -11,12 +11,31 @@ type Hello struct {
 	l *log.Logger
 }
 
+type Goodbye struct {
+	l *log.Logger
+}
+
 func NewHello(l *log.Logger) *Hello {
 	return &Hello{l}
 }
 
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
 func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	h.l.Println("hello world!")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "hello %s", d)
+}
+
+func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	g.l.Println("Goodbye!")
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, "Ooops", http.StatusBadRequest)
--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/zzibert/building-microservices-with-go/handlers"
 )
 
 func main() {
 
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gb := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gb)
 
 	http.ListenAndServe(":9090", sm)
 }
