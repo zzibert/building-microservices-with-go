04793fb04c02c280afe2f34b8db4b82d5821339f
Zan Zibert <zan_zibert@hotmail.com>
2020-08-10 16:34:16 +0200

    reverting to episode 1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/zzibert/building-microservices-with-go
 
 go 1.14
--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/zzibert/building-microservices-with-go/handlers"
 )
 
 func main() {
+	// reqeusts to the path /goodbye with be handled by this function
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
 
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
-	gh := handlers.NewGoodbye(l)
+	// any other request will be handled by this function
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Running Hello Handler")
 
-	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+		// read the body
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println("Error reading body", err)
 
-	http.ListenAndServe(":9090", sm)
+			http.Error(rw, "Unable to read request body", http.StatusBadRequest)
+			return
+		}
+
+		// write the response
+		fmt.Fprintf(rw, "Hello %s", b)
+	})
+
+	// Listen for connections on all ip addresses (0.0.0.0)
+	// port 9090
+	log.Println("Starting Server")
+	err := http.ListenAndServe(":9090", nil)
+	log.Fatal(err)
 }
