a0938b6ac8c4722e1b38263a6c13f5da0091d665
Zan Zibert <zan_zibert@hotmail.com>
2020-08-10 16:56:03 +0200

    succesfully implemented goodbye handler

---
+++ handlers/goodbye.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	g.l.Println("Goodbye world!")
+
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(rw, "Goodbye %s", d)
+}
