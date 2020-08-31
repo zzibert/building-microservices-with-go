b480b5c3b2cf6f44d891a7e69ff4956e4d6ac268
Zan Zibert <zan_zibert@hotmail.com>
2020-08-31 14:29:12 +0200

    revisiting building microservices ep. 1

--- main.go
+++ main.go
@@ -1,81 +1,28 @@
 package main
 
 import (
-	"context"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"time"
-
-	"github.com/go-openapi/runtime/middleware"
-	"github.com/gorilla/mux"
-	"github.com/zzibert/building-microservices-with-go/handlers"
 )
 
 func main() {
-
-	l := log.New(os.Stdout, "products-api ", log.LstdFlags)
-
-	// create the handlers
-	ph := handlers.NewProducts(l)
-
-	// create a new serve mux and register the handlers
-	sm := mux.NewRouter()
-
-	getRouter := sm.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/", ph.GetProducts)
-
-	putRouter := sm.Methods(http.MethodPut).Subrouter()
-	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
-	putRouter.Use(ph.MiddlewareValidateProduct)
-
-	postRouter := sm.Methods(http.MethodPost).Subrouter()
-	postRouter.HandleFunc("/", ph.AddProduct)
-	postRouter.Use(ph.MiddlewareValidateProduct)
-
-	deleteRouter := sm.Methods(http.MethodDelete).Subrouter()
-	deleteRouter.HandleFunc("/{id:[0-9]+}", ph.DeleteProduct)
-
-	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
-	sh := middleware.Redoc(opts, nil)
-
-	getRouter.Handle("/docs", sh)
-	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
-
-	//sm.Handle("/products", ph)
-
-	// create a new server
-	s := http.Server{
-		Addr:         ":9090",           // configure the bind address
-		Handler:      sm,                // set the default handler
-		ErrorLog:     l,                 // set the logger for the server
-		ReadTimeout:  5 * time.Second,   // max time to read request from the client
-		WriteTimeout: 10 * time.Second,  // max time to write response to the client
-		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
-	}
-
-	// start the server
-	go func() {
-		l.Println("Starting server on port 9090")
-
-		err := s.ListenAndServe()
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		data, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			l.Printf("Error starting server: %s\n", err)
-			os.Exit(1)
+			http.Error(rw, "00000sps", http.StatusBadRequest)
+			return
 		}
-	}()
 
-	// trap sigterm or interupt and gracefully shutdown the server
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	signal.Notify(c, os.Kill)
+		fmt.Fprintf(rw, "Hello %s", data)
 
-	// Block until a signal is received.
-	sig := <-c
-	log.Println("Got signal:", sig)
+	})
 
-	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	s.Shutdown(ctx)
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
